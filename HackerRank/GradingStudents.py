59779d611aed25fa1798a38b115068ce116bde7d
yordanos <101830198+J0RdN-w@users.noreply.github.com>
2022-08-11 13:58:54 +0300

    Update GradingStudents.py

--- HackerRank/GradingStudents.py
+++ HackerRank/GradingStudents.py
@@ -1,61 +1,43 @@
-'use strict';
+#!/bin/python3
 
-const fs = require('fs');
+import math
+import os
+import random
+import re
+import sys
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
 
-let inputString = '';
-let currentLine = 0;
+def gradingStudents(grades):
+    
+    grade_res = []
+    for grade in grades:
+        next_mulof_5 = ((grade + 5) // 5) * 5
+        if next_mulof_5 - grade < 3 and grade >= 38:
+            grade_res.append(next_mulof_5)
+        else:
+            grade_res.append(grade)
+    return grade_res            
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+    grades_count = int(input().strip())
 
-    main();
-});
+    grades = []
 
-function readLine() {
-    return inputString[currentLine++];
-}
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
 
-/*
- * Complete the 'gradingStudents' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY grades as parameter.
- */
+    result = gradingStudents(grades)
 
-function gradingStudents(grades) {
-    // Write your code here
-    return grades.map(grade => {
-        let next_mul_of5 = parseInt((grade + 5) / 5) * 5;
-        if( (next_mul_of5 - grade) < 3 && grade >= 38)
-          return next_mul_of5
-        else 
-        return grade
-    })
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
 
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const gradesCount = parseInt(readLine().trim(), 10);
-
-    let grades = [];
-
-    for (let i = 0; i < gradesCount; i++) {
-        const gradesItem = parseInt(readLine().trim(), 10);
-        grades.push(gradesItem);
-    }
-
-    const result = gradingStudents(grades);
-
-    ws.write(result.join('\n') + '\n');
-
-    ws.end();
-}
+    fptr.close()
